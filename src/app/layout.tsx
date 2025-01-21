8e2e639efcb17210354e8c7d6581895a86711d1d
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-01-21 13:38:56 -0500

    Clean up build process

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
