b698a604ce5d63e7a91a5d79be60c03a74d21540
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-03-10 14:56:21 -0400

    Force dynamic, change vscode typescript version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
+      <div>Sign In</div>
     </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPLoDuA7Uki1dP5nRmTEOfp2beaBJqDNh47UvA",
   "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPxsB1MJPsFS2Z6u8Ic0NoBiDR9rbypEmHAtzX",
   "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPDgoQkoFu80MPSsHW7FEbCA3fY4rloVvdLwiX",
   "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPuooyXNXYCK3aqd9Ve7i5I2w4YXDJ1jUHyMfQ",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbP6YoToSaDnso10RGhtW6bzIVeUaFPdK7wiyCr",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPPMbCnpfc0b7zyokMgvBT629dpxXufZenCQJW",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPIBwZEqLH0DKfmO3eUxy1ujP4YbSWikNRElrV",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPzm5sBAZGmW5ECXIvR7TKbLP0eo16JDcyuZFB",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbP6kdfYXDnso10RGhtW6bzIVeUaFPdK7wiyCrL",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPSRXbReiBD2I4FLAPUThRjNb7flc51pgSqGdz",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPnGJYK6WwxIVPtdEflJnCXsMiFceOZj19KaSr",
-  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPoBIRzEFn9yqcsnh3Yzm8JTHdZ5uRtWUCFKew",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPxsB1MJPsFS2Z6u8Ic0NoBiDR9rbypEmHAtzX",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPDgoQkoFu80MPSsHW7FEbCA3fY4rloVvdLwiX",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPuooyXNXYCK3aqd9Ve7i5I2w4YXDJ1jUHyMfQ",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPxsB1MJPsFS2Z6u8Ic0NoBiDR9rbypEmHAtzX",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPDgoQkoFu80MPSsHW7FEbCA3fY4rloVvdLwiX",
+  "https://3mxg08wsuw.ufs.sh/f/GZaRrrX9CTbPuooyXNXYCK3aqd9Ve7i5I2w4YXDJ1jUHyMfQ",
 ]
 
 const mockImages = mockUrls.map((url, index) => ({
