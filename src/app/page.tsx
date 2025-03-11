a462ac976d750d43d7e735dba9f4cbe1c604c0d7
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-03-10 20:29:33 -0400

    Refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,12 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy:(model, { desc }) => desc(model.id),
   });
-
   return (
-    <main className="">
-      <SignedOut>
-        <div className="w-full h-full text-2xl text-center">Please sign in above.</div>
-      </SignedOut>
-      <SignedIn>
-        <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4">
           {[...images, ...images, ...images, ...images].map((image, index) => (
             <div key={image.id + '-' + index} className="w-48">
               <img src={image.url} alt="image" />
@@ -23,6 +17,17 @@ export default async function HomePage() {
             </div>
           ))}
         </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above.</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
       </SignedIn>
     </main>
   );
