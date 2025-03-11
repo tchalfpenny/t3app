6abc56341c8a4a0c3735a24221bae7e446e921e6
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-03-10 20:24:42 -0400

    Require auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -9,16 +10,20 @@ export default async function HomePage() {
   });
 
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + '-' + index} className="w-48">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-      Hello (gallery in progress)
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above.</div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4">
+          {[...images, ...images, ...images, ...images].map((image, index) => (
+            <div key={image.id + '-' + index} className="w-48">
+              <img src={image.url} alt="image" />
+              <div>{image.name}</div>
+            </div>
+          ))}
+        </div>
+      </SignedIn>
     </main>
   );
 }
